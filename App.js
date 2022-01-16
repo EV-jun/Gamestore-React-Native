21b81f575bb77a063a069364ebec22311ae7cbc5
Junfeng Ou <og@Junfengs-MacBook-Pro.local>
2022-01-16 11:41:05 -0600

    First commit

---
+++ App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import MaterialIcons from "react-native-vector-icons/MaterialIcons"
+ 
+ import {
+   StyleSheet,
+   View,
+   Text,
+   TouchableOpacity,
+   SafeAreaView
+ } from 'react-native';
+ 
+ const App =() => {
+   return (
+     <SafeAreaView style={styles.safeArea}>
+       <View>
+         <Text style={{fontSize: 30, fontWeight: 'bold', color: '#20315f'}}>hmm</Text>
+       </View>
+       <TouchableOpacity style={styles.actionBtn}>
+         <Text style={styles.actionBtnText}>Let's Begin</Text>
+         <MaterialIcons name="arrow-forward-ios" size={22} color="#fff" />
+       </TouchableOpacity>
+     </SafeAreaView>
+   )
+ };
+ 
+ const styles = StyleSheet.create({
+   safeArea: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+     backgroundColor: "#fff"
+   }, 
+   actionBtn: {
+     backgroundColor: "#AD40AF",
+     padding: 20,
+     width: "90%",
+     borderRadius: 5, 
+     flexDirection: "row",
+     justifyContent: "space-between",
+   },
+   actionBtnText: {
+     fontWeight: "bold",
+     fontSize: 18,
+     color: "#fff",
+     // fontFamily: "Roboto-MediumItalic"
+   }
+ });
+ 
+ export default App;
+ 
