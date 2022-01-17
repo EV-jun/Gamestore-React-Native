1bf192f6326bd0953df0db9796094e1e8ee0d7fd
Junfeng Ou <og@Junfengs-MacBook-Pro.local>
2022-01-16 22:10:36 -0600

    Added sidebar navigation screen

--- src/navigation/Appstack.js
+++ src/navigation/Appstack.js
@@ -1,13 +1,26 @@
 import React from 'react'
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+// import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import Home from "../screens/HomeScreen"
+import Message from "../screens/MessageScreen"
+import Moments from "../screens/MomentsScreen"
+import Profile from "../screens/ProfileScreen"
+import Settings from "../screens/SettingsScreen"
+import { createDrawerNavigator } from "@react-navigation/drawer"
+
+// Navigation Drawer that shows the side bar menu
 
 const Appstack = () => {
-    const Stack = createNativeStackNavigator();
+    // const Stack = createNativeStackNavigator();
+    const Drawer = createDrawerNavigator();
     return (
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-            <Stack.Screen component={Home} name="Home" />
-        </Stack.Navigator>
+        <Drawer.Navigator screenOptions={{headerShown: false}}>
+            <Drawer.Screen component={Home} name="Home" />
+            <Drawer.Screen component={Message} name="Message" />
+            <Drawer.Screen component={Moments} name="Moments" />
+            <Drawer.Screen component={Profile} name="Profile" />
+            <Drawer.Screen component={Settings} name="Settings" />
+            
+        </Drawer.Navigator>
     )
 }
 
