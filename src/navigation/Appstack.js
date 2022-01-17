aa332d2409d52b1c82be48986d2ad486483134d5
Junfeng Ou <og@Junfengs-MacBook-Pro.local>
2022-01-16 23:01:15 -0600

    Side menu fully functional

--- src/navigation/Appstack.js
+++ src/navigation/Appstack.js
@@ -6,6 +6,8 @@ import Moments from "../screens/MomentsScreen"
 import Profile from "../screens/ProfileScreen"
 import Settings from "../screens/SettingsScreen"
 import { createDrawerNavigator } from "@react-navigation/drawer"
+import CustomDrawer from '../components/CustomDrawer'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 // Navigation Drawer that shows the side bar menu
 
@@ -13,12 +15,66 @@ const Appstack = () => {
     // const Stack = createNativeStackNavigator();
     const Drawer = createDrawerNavigator();
     return (
-        <Drawer.Navigator screenOptions={{headerShown: false}}>
-            <Drawer.Screen component={Home} name="Home" />
-            <Drawer.Screen component={Message} name="Message" />
-            <Drawer.Screen component={Moments} name="Moments" />
-            <Drawer.Screen component={Profile} name="Profile" />
-            <Drawer.Screen component={Settings} name="Settings" />
+        <Drawer.Navigator 
+            drawerContent={props => <CustomDrawer {...props} />} 
+            screenOptions={{
+                headerShown: false, 
+                drawerLabelStyle: {marginLeft: -25, fontSize: 15},
+                drawerActiveBackgroundColor: '#aa18ea',
+                drawerActiveTintColor: '#fff',
+                drawerInactiveTintColor: '#333',
+            }}
+                
+        >
+            <Drawer.Screen 
+                component={Home} 
+                name="Home" 
+                options={{
+                    drawerIcon: ({color}) => (
+                      <Ionicons name="home-outline" size={22} color={color} />
+                    ),
+                  }}
+            />
+
+            <Drawer.Screen 
+                component={Message} 
+                name="Message"
+                options={{
+                    drawerIcon: ({color}) => (
+                      <Ionicons name="chatbox-ellipses-outline" size={22} color={color} />
+                    ),
+                  }}
+            />
+
+            <Drawer.Screen
+                component={Moments} 
+                name="Moments" 
+                options={{
+                    drawerIcon: ({color}) => (
+                      <Ionicons name="timer-outline" size={22} color={color} />
+                    ),
+                  }}
+            />
+
+            <Drawer.Screen 
+                component={Profile} 
+                name="Profile"
+                options={{
+                    drawerIcon: ({color}) => (
+                      <Ionicons name="person-outline" size={22} color={color} />
+                    ),
+                  }}
+            />
+
+            <Drawer.Screen 
+                component={Settings} 
+                name="Settings" 
+                options={{
+                    drawerIcon: ({color}) => (
+                      <Ionicons name="settings-outline" size={22} color={color} />
+                    ),
+                  }}
+            />
             
         </Drawer.Navigator>
     )
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -25,7 +25,10 @@ const HomeScreen = ({navigation}) => {
             <ScrollView style={{padding: 20}}>
                 <View style={{flexDirection: "row", justifyContent: "space-between", marginBottom: 20}}>
                     <Text style={{fontSize:16}}>Hello John</Text>
-                    <TouchableOpacity onPress={() => navigation.goBack()} >
+                    <TouchableOpacity onPress={
+                        // () => navigation.goBack()
+                        () => navigation.openDrawer()
+                    } >
                         <ImageBackground 
                             source={require("../assets/images/user-profile.jpg")}
                             style={{width: 35, height: 35}}  
